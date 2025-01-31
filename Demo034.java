0905f4d05432a6f7a3bfe511fe734eff277b1c88
kiran0415555 <udaypalleboina7@gmail.com>
2025-01-31 11:08:56 +0530

    Add files via upload

--- Demo034.java
+++ Demo034.java
@@ -1,5 +1,15 @@
-package Core_java;
+import java.util.Scanner;
 
-public class Demo034 {
-
+class Demo034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int number = sc.nextInt();
+        if(number%2==0){
+            System.out.println("The"+number+"is even");
+        }
+        else{
+            System.out.println("The "+number+" is odd");
+        }
+    }
 }
